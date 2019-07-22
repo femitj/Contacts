28eb9d1dfa3305ec1bb751b4b43e4f6abbf55c53
Tijani Oluwafemi <olutimedia@gmail.com>
2019-07-22 12:34:06 +0100

    Updates

---
+++ precache-manifest.df6394a9ea943ee45925d5863a6fb68e.js
@@ -0,0 +1,42 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "e46fbe5630c8f46b9445fb2ecaa1b741",
+    "url": "/Contacts/index.html"
+  },
+  {
+    "revision": "c098ab6dc68ad9aada99",
+    "url": "/Contacts/static/css/main.983af21d.chunk.css"
+  },
+  {
+    "revision": "3ec620609d36f1ac0c4b",
+    "url": "/Contacts/static/js/2.0812122e.chunk.js"
+  },
+  {
+    "revision": "c098ab6dc68ad9aada99",
+    "url": "/Contacts/static/js/main.ee5b8f29.chunk.js"
+  },
+  {
+    "revision": "c77fbdc446c95c69bb0e",
+    "url": "/Contacts/static/js/runtime~main.cb207ace.js"
+  },
+  {
+    "revision": "c0c5c509a7483ce0b8c11ca04f3633f1",
+    "url": "/Contacts/static/media/arrow-back.c0c5c509.svg"
+  },
+  {
+    "revision": "32cee4372a4c9744f7e703013278c61d",
+    "url": "/Contacts/static/media/cancel.32cee437.svg"
+  },
+  {
+    "revision": "e24cad8280195f088bd3bfb93f09e582",
+    "url": "/Contacts/static/media/person-add.e24cad82.svg"
+  },
+  {
+    "revision": "198acd46307369567e1e29a42be518d9",
+    "url": "/Contacts/static/media/person.198acd46.svg"
+  },
+  {
+    "revision": "b884e292f0f2f394e31cd93106b0386e",
+    "url": "/Contacts/static/media/search.b884e292.svg"
+  }
+]);
--- service-worker.js
+++ service-worker.js
@@ -14,7 +14,7 @@
 importScripts("https://storage.googleapis.com/workbox-cdn/releases/4.3.1/workbox-sw.js");
 
 importScripts(
-  "/precache-manifest.71b7cd9896f889567d808378459ff769.js"
+  "/Contacts/precache-manifest.df6394a9ea943ee45925d5863a6fb68e.js"
 );
 
 self.addEventListener('message', (event) => {
@@ -33,7 +33,7 @@ workbox.core.clientsClaim();
 self.__precacheManifest = [].concat(self.__precacheManifest || []);
 workbox.precaching.precacheAndRoute(self.__precacheManifest, {});
 
-workbox.routing.registerNavigationRoute(workbox.precaching.getCacheKeyForURL("/index.html"), {
+workbox.routing.registerNavigationRoute(workbox.precaching.getCacheKeyForURL("/Contacts/index.html"), {
   
   blacklist: [/^\/_/,/\/[^\/]+\.[^\/]+$/],
 });
